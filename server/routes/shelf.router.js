3ae59d2519173240bf51bd9e0e3296e2048038d7
Cassidy Foust <cassidy.foust@gmail.com>
2019-10-10 13:52:32 -0500

    added saga and reducer

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const router = express.Router();
 
 /**
  * Get all of the items on the shelf
  */
-router.get('/', (req, res) => {
-    res.sendStatus(200); // For testing only, can be removed
+router.get('/', rejectUnauthenticated, (req, res) => {
+    pool.query('SELECT * FROM "item";')
+        .then(results => res.send(results.rows))
+        .catch(error => {
+            console.log('Error making SELECT for items:', error);
+            res.sendStatus(500);
+        });// For testing only, can be removed
 });
-
 
 /**
  * Add an item for the logged in user to the shelf
